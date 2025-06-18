06a66d9f3e5a8bb62a8e39c0fac7c11e7bde64f0
David Schmidt <deschmidt@detr.nv>
2025-06-18 15:43:05 -0700

    Add simplify_table to get_ces.

--- R/get_ces.R
+++ R/get_ces.R
@@ -7,8 +7,9 @@
 #' @param transform Logical. If TRUE (default), converts employment values from thousands
 #'   to actual counts by multiplying by 1000 for specific data types (codes 1, 6, 26)
 #'   and removes ", In Thousands" from data type labels.
-#' @param monthly_only Logical. If TRUE (default), filters out annual data (period M13)
-#'   and creates a date column from year and period.
+#' @param monthly_only Logical. If TRUE (default), filters out annual data (period M13).
+#' @param simplify_table Logical. If TRUE (default), removes excess columns and creates 
+#'   a date column from Year and Period in the original data.   
 #'
 #' @return A data.table containing CES data with the following key columns:
 #'   \describe{
@@ -16,7 +17,7 @@
 #'     \item{year}{Year of observation}
 #'     \item{period}{Time period (M01-M12 for months, M13 for annual)}
 #'     \item{value}{Employment statistic value}
-#'     \item{date}{Date column (if monthly_only = TRUE)}
+#'     \item{date}{Date column (if simplify_table = TRUE)}
 #'     \item{industry_text}{Industry description}
 #'     \item{state_text}{State name}
 #'     \item{area_text}{Area description}
@@ -35,22 +36,22 @@
 #' # Include annual data
 #' ces_all <- get_ces(monthly_only = FALSE)
 #' }
-get_ces <- function(transform = TRUE, monthly_only = TRUE) {
+get_ces <- function(transform = TRUE, monthly_only = TRUE, simplify_table = TRUE) {
 
-  ces_import <- fread_bls("https://download.bls.gov/pub/time.series/sm/sm.data.1.AllData") %>% select(-footnote_codes) %>%
-    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.series"), by = "series_id") %>%  select(-footnote_codes) %>%
-    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.industry"), by = "industry_code") %>%
-    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.state"), by = "state_code") %>%
-    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.area"), by = "area_code") %>%
-    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.data_type"), by = "data_type_code") %>%
-    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.supersector"), by = "supersector_code") %>%
+  ces_import <- fread_bls("https://download.bls.gov/pub/time.series/sm/sm.data.1.AllData") |> select(-footnote_codes) |>
+    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.series"), by = "series_id") |>  select(-footnote_codes) |>
+    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.industry"), by = "industry_code") |>
+    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.state"), by = "state_code") |>
+    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.area"), by = "area_code") |>
+    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.data_type"), by = "data_type_code") |>
+    left_join(fread_bls("https://download.bls.gov/pub/time.series/sm/sm.supersector"), by = "supersector_code") |>
     mutate(value = as.numeric(value),
-           industry_code = substr(series_id,11,18)) %>%
+           industry_code = substr(series_id,11,18)) |>
     filter(!is.na(value))
 
   if(transform){
 
-    ces_import <- ces_import %>%
+    ces_import <- ces_import |>
       mutate(
         value = if_else(
           data_type_code %in% c(1,6,26),
@@ -64,10 +65,16 @@ get_ces <- function(transform = TRUE, monthly_only = TRUE) {
 
   if(monthly_only){
 
-    ces_import <- ces_import %>%
-      filter(period != "M13") %>%
-      mutate(date = lubridate::ym(paste(as.character(year),substr(period,2,3),sep = "-")))
-
+    ces_import <- ces_import |>
+      filter(period != "M13")
+  }
+  
+  if(simplify_table){
+    
+    ces_import <- ces_import |>
+      mutate(date = lubridate::ym(paste0(year,period))) |>
+      select(-c(benchmark_year:end_period,year,period)) |>
+      filter(state_code != 0)
   }
 
   return(ces_import)
