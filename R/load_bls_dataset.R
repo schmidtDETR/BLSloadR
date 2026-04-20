426fc4150e4fb3d803398749919dd800f48912f8
David Schmidt <deschmidt@detr.nv>
2026-04-20 09:18:01 -0700

    Example set to dontrun and implementation of get_bls_headers added to load_bls_dataset.

--- R/load_bls_dataset.R
+++ R/load_bls_dataset.R
@@ -53,7 +53,7 @@
 #' @importFrom utils head
 #' 
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' # Import All Data
 #' fm_import <- load_bls_dataset("fm", which_data = "all")
 #' 
@@ -107,27 +107,16 @@ load_bls_dataset <- function(database_code, return_full = FALSE, simplify_table 
   get_directory_files <- function(url, prefix) {
     tryCatch({
       # Set up headers to avoid 403 errors
-      headers <- c(
-        "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding" = "gzip, deflate, br",
-        "Accept-Language" = "en-US,en;q=0.9",
-        "Connection" = "keep-alive",
-        "Host" = "download.bls.gov",
-        "Referer" = "https://download.bls.gov/pub/time.series/",
-        "Sec-Ch-Ua" = 'Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-        "Sec-Ch-Ua-Mobile" = "?0",
-        "Sec-Ch-Ua-Platform" = '"Windows"',
-        "Sec-Fetch-Dest" = "document",
-        "Sec-Fetch-Mode" = "navigate",
-        "Sec-Fetch-Site" = "same-origin",
-        "Sec-Fetch-User" = "?1",
-        "Upgrade-Insecure-Requests" = "1",
-        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-      )
+      headers <- get_bls_headers()
       
       # Make request with headers
       response <- httr::GET(url, httr::add_headers(.headers = headers))
       httr::stop_for_status(response)
+      
+      # Exit function if download failed.
+      if(is.null(downloads)){
+        stop("Download of BLS data failed.  Please run with suppress_warnings = FALSE for additional status messages. Consider setting the BLS_USER_AGENT environment variable to your email address to avoid Status 403 errors from BLS.")
+      }
       
       # Parse HTML content
       page <- rvest::read_html(httr::content(response, as = "text"))
